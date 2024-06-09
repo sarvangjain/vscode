4d2798cf6ff7101a3bad4115566aede8623953b8
Sarvang Jain <41965874+sarvangjain@users.noreply.github.com>
2024-06-09 20:18:37 +0000

    Add sample.csv

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     csv_file_path = 'sample.csv'
     main(csv_file_path)
 
-    mainzk()
+    # mainzk()
     # zk = KazooClient(hosts='127.0.0.1:2181')
     # zk.start()
     
